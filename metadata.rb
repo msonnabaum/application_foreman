629c74cccc6bc46258131fc343b22358cebef26d
Mark Sonnabaum <mark@sonnabaum.com>
2014-02-01 16:38:26 -0600

    Bumped application dependency.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,4 @@ description      "Provides a foreman_export LWRP for exporting init config."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-%w{ application }.each do |cb|
-  depends cb
-end
+depends "application", ">= 3.0.0"
